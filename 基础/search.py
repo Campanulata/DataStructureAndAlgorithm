1815457d82470e7052d9db9e511b7d7f3cee2cf0
Campanulata <ts1989512@outlook.com>
2020-07-01 19:44:26 +0800

    1

--- 基础/search.py
+++ 基础/search.py
@@ -1,9 +1,26 @@
-def linear_search(li,val):
-  for ind,v in enumerate(li):
-    if  v==val:
-      return ind
-    else:
-      return None
+def linear_search(li, val):
+    for ind, v in enumerate(li):
+        if v == val:
+        	return ind
+    return None
 
-seq = ['one', 'two', 'three']
-print(linear_search(seq,'one'))
+def binary_search(li,tar):
+	left=0
+	right=len(li)-1
+	
+	while(left<=right):
+		mid=(left+right)//2
+		if li[mid]==tar:
+			return mid
+		elif li[mid]<tar:
+			left=mid+1
+		else:
+			right=mid-1
+	return None
+
+
+
+
+seq = [1, 2, 3, 4,5]
+print(linear_search(seq,2))
+print(binary_search(seq,2))
